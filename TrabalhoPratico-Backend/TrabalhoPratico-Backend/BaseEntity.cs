688ca9de7921b3063e00427e25b3a9e0115fbda9
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2022-07-26 22:25:05 -0300

    Aplicar Seed e Migration para banco de dados

--- TrabalhoPratico-Backend/TrabalhoPratico-Backend/BaseEntity.cs
+++ TrabalhoPratico-Backend/TrabalhoPratico-Backend/BaseEntity.cs
@@ -6,6 +6,6 @@ namespace TrabalhoPratico_Backend
     {
         public virtual Guid Id { get; set; }
         public string Name { get; set; }
-        public bool Deletada { get; set; }
+        public bool Deletada { get; set; } = false;
     }
 }
--- TrabalhoPratico-Backend/TrabalhoPratico-Backend/Entities/Book.cs
+++ TrabalhoPratico-Backend/TrabalhoPratico-Backend/Entities/Book.cs
@@ -6,12 +6,28 @@ namespace Entities
 {
     public class Book : BaseEntity
     {
-        public List<Author> Id_Author { get; set; }
         public string Publisher { get; set; }
         public string Language { get; set; }
         public int Pages { get; set; }
         public int Edition { get; set; }
         public int Publication { get; set; }
         public string Description { get; set; }
+
+        private List<Author> _authors = new();
+        public IEnumerable<Author> Authors => _authors;
+
+        public Book() { }
+
+        public Book(string name, string publisher, string language, int pages, int edition, int publication, string description)
+        {
+            Name = name;
+            Publisher = publisher;
+            Language = language;
+            Pages = pages;
+            Edition = edition;
+            Publication = publication;
+            Description = description;
+        }
+
     }
 }
